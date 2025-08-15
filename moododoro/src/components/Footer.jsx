f11664af67094a09e39ee13655b2777fc3468d40
Matt Schwartz <matschwartz6.0@gmail.com>
2025-08-15 01:52:18 -0400

    Updates

--- moododoro/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-gray-800 text-white py-4 text-center">
-        <p>© Matt Schwartz & Allie Kuo, 2025</p>
-    </footer>
-  )
-}
-
-export default Footer
